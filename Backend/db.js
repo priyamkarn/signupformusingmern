2092a905aa7ec2207fe471e8edb321c4b84b3b7e
priyam karn <83950448+priyamkarn@users.noreply.github.com>
2024-08-14 21:19:00 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const connectionString = 'mongodb+srv://priyamkarn54:altruist@cluster0.ckxgc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const connectionString = 'db_url';
 
 // Connect to MongoDB
 mongoose.connect(connectionString)
